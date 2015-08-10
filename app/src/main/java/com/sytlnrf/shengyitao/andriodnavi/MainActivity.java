accf5967f33ba0939efea90c14f14254dfc1f8f8
sytlnrf <sheng_yitao@163.com>
2015-08-10 23:00:00 +0800

    add button & listener

--- app/src/main/java/com/sytlnrf/shengyitao/andriodnavi/MainActivity.java
+++ app/src/main/java/com/sytlnrf/shengyitao/andriodnavi/MainActivity.java
@@ -4,15 +4,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mtrueButon;
+    Button mfalseButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        mtrueButon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT).show();
+            }
+        });
+        mfalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,R.string.wrong_toast,Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
+    private void initView(){
+        mtrueButon = (Button)findViewById(R.id.true_button);
+        mfalseButton = (Button)findViewById(R.id.false_button);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
